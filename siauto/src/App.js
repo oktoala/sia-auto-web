6315566e2c3c221ead30ae853ca3fe22a226ad86
oktoala <okta.yoga.syah@gmail.com>
2021-08-20 14:54:32 +0800

    Add Form

--- siauto/src/App.js
+++ siauto/src/App.js
@@ -1,11 +1,13 @@
 import unmul from './img/unmul.png';
-import { Button, Alert } from 'react-bootstrap';
+import { Button, Alert, Form, FormGroup } from 'react-bootstrap';
 
 function App() {
   return (
     <div className="App">
-      <Header/>
-      <MainForm/>
+      <Header />
+      <Main>
+        <MainSection></MainSection>
+      </Main>
     </div>
   );
 }
@@ -15,7 +17,7 @@ function Header() {
     <header className="header">
       <div className="header_container">
         <div className="logo">
-          <a className="logo"href="/">
+          <a className="logo" href="/">
             <div className="logo_image">
               <img width="50px" src={unmul} alt="logo"></img>
             </div>
@@ -32,15 +34,48 @@ function Header() {
   );
 }
 
-function MainForm() {
+function Main(props) {
   return (
-    <section>
-      <main>
-      <Button>Makan Dulu</Button>
-      </main>
+    <main>
+      {props.children}
+    </main>
+  );
+}
+
+function MainSection() {
+  return (
+    <section className="main-section">
+      <Form>
+        <FormGroups type="nim">NIM</FormGroups>
+        <FormGroups type="password">Password</FormGroups>
+      </Form>
+      <Form.Label>Nilai Kuesioner</Form.Label>
+      <Form.Group className="mb-3" controlId="basicFormCheckbox">
+        <CheckBoxes label="1" />
+        <CheckBoxes label="2" />
+      </Form.Group>
+      <Button variant="primary" type="submit">Mulai</Button>
     </section>
   );
 }
 
+
+
+function FormGroups(props) {
+  return (
+    <Form.Group className="mb-3" controlId={`basicForm${props.children}`}>
+      <Form.Label>{props.children}</Form.Label>
+      <Form.Control type={props.type} placeholder={`Masukkan ${props.children}`} />
+    </Form.Group>
+  );
+}
+
+function CheckBoxes(props) {
+  return (
+    <Form.Check inline label={props.label} name="group1"
+      type="checkbox" id={`inline-checkbox-${props.label}`} />
+
+  );
+}
 
 export default App;
